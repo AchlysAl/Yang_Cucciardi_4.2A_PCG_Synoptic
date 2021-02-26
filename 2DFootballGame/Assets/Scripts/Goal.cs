06ec504b3ead40564fb77d1bf2da9a0ea5924283
AchlysAl <73109754+AchlysAl@users.noreply.github.com>
2021-02-26 20:09:09 +0100

    changes

---
+++ 2DFootballGame/Assets/Scripts/Goal.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip clip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadLevelDelay());
+
+        Destroy(collision.gameObject);
+    }
+
+     IEnumerator LoadLevelDelay()
+    {       
+        if (ScoreScript.scorevalue == 20)
+        {
+            yield return new WaitForSeconds(2);
+            SceneManager.LoadScene("WinScene");
+        }
+        else
+        {
+            ScoreScript.scorevalue += 1;
+
+            audioSource.PlayOneShot(clip);
+        }
+
+    }
+
+}
